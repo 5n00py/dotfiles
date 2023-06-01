40ff193d7807b6d58da7ec456fc0d884d24cae2c
David Schmid <david.schmid@mailbox.org>
2023-06-01 11:46:19 +0200

    Add and Update Python Aliases
    - Refactor pyfmt to format recursively all .py files in a project
    - Define pt alias for pytest with verbosity and doctests by default

--- zsh/sources/aliases/aliases-coding.zsh
+++ zsh/sources/aliases/aliases-coding.zsh
@@ -2,12 +2,14 @@
 alias pycov='python3 -m pytest --cov --cov-report=xml'
 
 # Formatting source code in python using black
-alias pyfmt='python3 -m black'
+alias pyfmt='find . -name "*.py" | xargs python3 -m black'
+
+# Alias for pytest with verbosity and doctest-modules
+alias pt='pytest -v --doctest-modules'
 
 # Formatting source code in a C project
 alias cfmt='find . -name "*.c" -o -name "*.h" | xargs clang-format -style=file:~/.clang-format -i'
 
 # Formatting rust code in a Rust project
 alias rfmt='find . -name "*.rs" | xargs rustfmt'
-
 
