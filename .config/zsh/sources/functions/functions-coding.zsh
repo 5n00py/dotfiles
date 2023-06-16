5f9a79febe5e83fe0ed8165fadd658443b6ba2dd
David Schmid <david.schmid@mailbox.org>
2023-06-17 00:15:23 +0200

    Refactor venv activation and deactivation, add recursive search
    This commit refactors the venv() function to improve the activation and
    deactivation of Python virtual environments.
    
    Additionally, a helper function named find_venv() has been introduced to
    perform a recursive search for the venv directory.

--- .config/zsh/sources/functions/functions-coding.zsh
+++ .config/zsh/sources/functions/functions-coding.zsh
@@ -1,21 +1,31 @@
-# Activate Python virtual environment
-# This function will activate the first venv it finds in the current directory 
-# or any subdirectories. If no venv is found, it will print a message saying so.
-# Note: This function only activates the first venv directory it finds. If
-# there are multiple venv dirs in a project, an other appreach might be needed.
-activate_venv() {
-    # Recursively finds a 'venv' directory
-    venv_path=$(find . -name venv)
+# Toggle activation and deactivation of a Python virtual environment
+venv() {
+  # Helper function to search for "venv" directory recursively
+  find_venv() {
+    local current_dir="$1"
+    while [[ "$current_dir" != "/" ]]; do
+      if [[ -d "$current_dir/venv" ]]; then
+        echo "$current_dir/venv"
+        return
+      fi
+      current_dir=$(dirname "$current_dir")
+    done
+  }
 
-    # If venv_path is not empty
-    if [[ -n $venv_path ]]; then
-        # Activates the virtual environment
-        source $venv_path/bin/activate
-        echo "Activated virtual environment at $venv_path"
+  if [[ -z $VIRTUAL_ENV ]]; then
+    local venv_dir=$(find_venv "$(pwd)")
+    if [[ -n $venv_dir ]]; then
+      source "$venv_dir/bin/activate"
+      echo "Virtual environment activated: $venv_dir"
     else
-        echo "No 'venv' directory found."
+      echo "No virtual environment found."
     fi
+  else
+    deactivate
+    echo "Virtual environment deactivated."
+  fi
 }
+
 
 # Define a function named "grip_preview" that takes a file as an argument$
 # renders the markdown and opens it in the default brosers
