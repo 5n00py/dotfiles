9a3eb1598706fd5f59eceaa961d475abc6410512
David Schmid <david.schmid@mailbox.org>
2023-06-24 12:13:11 +0200

    Refactor and Expand Aliases
    - Improve the pipupgrade alias to upgrade all outdated Python packages
    - Introduce the mkvenv alias to create a Python virtual environment named 'venv'
    - Update the yt alias to use yt-dlp for downloading YouTube videos
    - Enhance the ytm alias to download YouTube audio and convert it to MP3 format
    - Refactor and improve the cfmt alias for formatting C project source code
    - Update the rfmt alias for formatting Rust code in a Rust project
    - Add the pdfopen alias for opening PDF files with zathura
    - Introduce the bt alias for quick access to the Bluetooth control tool with root privileges

--- .config/zsh/sources/aliases/aliases-misc.zsh
+++ .config/zsh/sources/aliases/aliases-misc.zsh
@@ -1,12 +1,15 @@
 # Open PDF file with zathura
 alias pdfopen='zathura'
 
-# Download YouTube video with youtube-dl
+# Download YouTube video with yt-dlp
 # Usage: yt [youtube_url]
 # This will download the video at [youtube_url].
-alias yt='youtube-dl'
+alias yt='yt-dlp'
 
 # Download YouTube music with yt-dlp
 # Usage: ytm [youtube_url]
 # This will download the audio of the video at [youtube_url] and convert it to mp3 format.
-alias ytm='youtube-dl -x --audio-format mp3'
+alias ytm='yt-dlp -x --audio-format mp3'
+
+# Quickly access Bluetooth control tool with root privileges
+alias bt='sudo bluetoothctl'
