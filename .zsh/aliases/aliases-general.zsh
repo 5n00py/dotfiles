dffe95b8061c0c7762383670cdb58cb454e5c262
David Schmid <david.schmid@mailbox.org>
2023-06-27 10:28:29 +0200

    Update general aliases in .zsh configuration
    - Add "off" alias to quickly shut down the sytem
    - Add "rb" alias  to reboot the system
    - Add "out" alias to sign out from the i3 wm

--- .zsh/aliases/aliases-general.zsh
+++ .zsh/aliases/aliases-general.zsh
@@ -1,24 +1,35 @@
 # Enhanced listing: show hidden files, human-readable sizes, and list in long format
-alias ll='ls -lah'
+alias ll="ls -lah"
 
 # Colorize grep search output
-alias grep='grep --color=auto'
+alias grep="grep --color=auto"
 
 # Make directories, including necessary parent directories, and display verbose output
-alias mkdir='mkdir -pv'
+alias mkdir="mkdir -pv"
 
 # Use colordiff, a tool that colorizes diff output, as the default diff command
-alias diff='colordiff' # requires colordiff package
+alias diff="colordiff" # requires colordiff package
 
 # Search command history
-alias histg='history | grep' # search history
+alias histg="history | grep" # search history
 
 # Shortcuts for navigating up directory levels
-alias ..='cd ..'              # Go up one directory level
-alias ...='cd ../..'          # Go up two directory levels
-alias ....='cd ../../..'      # Go up three directory levels
-alias .....='cd ../../../..'  # Go up four directory levels
+alias ..="cd .."              # Go up one directory level
+alias ...="cd ../.."          # Go up two directory levels
+alias ....="cd ../../.."      # Go up three directory levels
+alias .....="cd ../../../.."  # Go up four directory levels
 
-alias browser='qutebrowser'
+# Launch Qutebrowser
+alias browser="qutebrowser"
 
-alias c='clear'
+# Clear the terminal
+alias c="clear"
+
+# Quickly shutting down the system
+alias off="sudo shutdown now"
+
+# Rebooting the system
+alias rb="sudo reboot"
+
+# Signing out from the i3 wm
+alias out="i3-msg exit"
